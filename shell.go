49b88d5cc4124956017c93452f510dae861a6335
Alexander Vielimchanitsia <alexandervielimchanitsia@gmail.com>
2016-02-11 14:21:11 +0200

    added PutObject func

--- shell.go
+++ shell.go
@@ -421,6 +421,40 @@ func (s *Shell) NewObject(template string) (string, error) {
 	return out.Hash, nil
 }
 
+func (s *Shell) PutObject(encoding string, r io.Reader) (string, error) {
+	var rc io.ReadCloser
+	if rclose, ok := r.(io.ReadCloser); ok {
+		rc = rclose
+	} else {
+		rc = ioutil.NopCloser(r)
+	}
+
+	// handler expects an array of files
+	fr := files.NewReaderFile("", "", rc, nil)
+	slf := files.NewSliceFile("", "", []files.File{fr})
+	fileReader := files.NewMultiFileReader(slf, true)
+
+	req := NewRequest(s.url, "object/put", encoding)
+	req.Body = fileReader
+
+	resp, err := req.Send(s.httpcli)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Close()
+	if resp.Error != nil {
+		return "", resp.Error
+	}
+
+	var out object
+	err = json.NewDecoder(resp.Output).Decode(&out)
+	if err != nil {
+		return "", err
+	}
+
+	return out.Hash, nil
+}
+
 func (s *Shell) ResolvePath(path string) (string, error) {
 	resp, err := s.newRequest("object/stat", path).Send(s.httpcli)
 	if err != nil {
