1426c60ac6a9fed1a9377836829b16a8945284d5
Steven Allen <steven@stebalien.com>
2018-08-24 14:16:34 -0700

    fix typed-nil error
    fixes #117

--- shell.go
+++ shell.go
@@ -541,7 +541,10 @@ func (s *Shell) PubSubPublish(topic, data string) (err error) {
 		return err
 	}
 	defer resp.Close()
-	return resp.Error
+	if resp.Error != nil {
+		return resp.Error
+	}
+	return nil
 }
 
 type ObjectStats struct {
