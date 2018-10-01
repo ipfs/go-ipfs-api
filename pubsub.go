dcf4c3fd318bd8cf15b2d421a596b3f0416ad09e
Steven Allen <steven@stebalien.com>
2018-10-01 12:54:32 -0700

    remove the go-libp2p-pubsub dependency
    We don't actually need to use the struct definitions from go-libp2p-pubsub. The
    interface is part of the API so we should be able to just re-implement them here.
    
    This *is* a breaking change: I've removed the interface and replaced it with a
    bare struct. IMO, that's *significantly* more idiomatic. `SeqNo()` needed to be
    changed *anyways* as we can't guarantee that sequence numbers are *just*
    numbers, they can be anything. The existing code would have crashed when run
    against js-ipfs, as far as I could tell.

--- pubsub.go
+++ pubsub.go
@@ -1,57 +1,17 @@
 package shell
 
 import (
-	"encoding/binary"
 	"encoding/json"
 
 	"github.com/libp2p/go-libp2p-peer"
-	pb "github.com/libp2p/go-libp2p-pubsub/pb"
 )
 
-// PubSubRecord is a record received via PubSub.
-type PubSubRecord interface {
-	// From returns the peer ID of the node that published this record
-	From() peer.ID
-
-	// Data returns the data field
-	Data() []byte
-
-	// SeqNo is the sequence number of this record
-	SeqNo() int64
-
-	//TopicIDs is the list of topics this record belongs to
-	TopicIDs() []string
+type Message struct {
+	From     peer.ID  `json:"from,omitempty"`
+	Data     []byte   `json:"data,omitempty"`
+	Seqno    []byte   `json:"seqno,omitempty"`
+	TopicIDs []string `json:"topicIDs,omitempty"`
 }
-
-type message struct {
-	*pb.Message
-}
-
-func (m *message) GetFrom() peer.ID {
-	return peer.ID(m.Message.GetFrom())
-}
-
-type floodsubRecord struct {
-	msg *message
-}
-
-func (r floodsubRecord) From() peer.ID {
-	return r.msg.GetFrom()
-}
-
-func (r floodsubRecord) Data() []byte {
-	return r.msg.GetData()
-}
-
-func (r floodsubRecord) SeqNo() int64 {
-	return int64(binary.BigEndian.Uint64(r.msg.GetSeqno()))
-}
-
-func (r floodsubRecord) TopicIDs() []string {
-	return r.msg.GetTopicIDs()
-}
-
-///
 
 // PubSubSubscription allow you to receive pubsub records that where published on the network.
 type PubSubSubscription struct {
@@ -67,17 +27,17 @@ func newPubSubSubscription(resp *Response) *PubSubSubscription {
 }
 
 // Next waits for the next record and returns that.
-func (s *PubSubSubscription) Next() (PubSubRecord, error) {
+func (s *PubSubSubscription) Next() (*Message, error) {
 	if s.resp.Error != nil {
 		return nil, s.resp.Error
 	}
 
 	d := json.NewDecoder(s.resp.Output)
 
-	r := &message{}
-	err := d.Decode(r)
+	var r Message
+	err := d.Decode(&r)
 
-	return floodsubRecord{msg: r}, err
+	return &r, err
 }
 
 // Cancel cancels the given subscription.
