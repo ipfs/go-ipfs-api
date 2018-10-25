fd84eb685d9ff063ac96e3bd75139cc73f0f3424
Thomas O'Brien <tbobrien612@gmail.com>
2018-10-25 14:01:42 +0200

    Set shellUrl back to localhost

--- shell_test.go
+++ shell_test.go
@@ -15,7 +15,7 @@ import (
 
 const (
 	examplesHash = "QmS4ustL54uo8FzR9455qaxZwuMiUhyvMcX9Ba8nUH4uVv"
-	shellUrl     = "https://ipfs.infura.io:5001"
+	shellUrl     = "localhost:5001"
 )
 
 func TestAdd(t *testing.T) {
