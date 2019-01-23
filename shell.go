056bd600cf6b7a969d1c838d3bf6e8012d54ea8f
Robert Lin <robertlin1@gmail.com>
2019-01-22 18:14:25 -0800

    shell: add NewDirectShell API

--- shell.go
+++ shell.go
@@ -72,6 +72,25 @@ func NewShell(url string) *Shell {
 	return NewShellWithClient(url, c)
 }
 
+// NewDirectShell creates a new shell that directly uses the provided URL,
+// instead of attempting to parse it into a multiaddr.
+//
+// For Nexus-hosted IPFS nodes, for example, use:
+//
+//     shell := NewDirectShell(fmt.Sprintf("nexus.temporal.cloud/network/%s", networkName))
+//
+func NewDirectShell(url string) *Shell {
+	return &Shell{
+		url: url,
+		httpcli: &gohttp.Client{
+			Transport: &gohttp.Transport{
+				Proxy:             gohttp.ProxyFromEnvironment,
+				DisableKeepAlives: true,
+			},
+		},
+	}
+}
+
 func NewShellWithClient(url string, c *gohttp.Client) *Shell {
 	if a, err := ma.NewMultiaddr(url); err == nil {
 		_, host, err := manet.DialArgs(a)
