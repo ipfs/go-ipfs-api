c8d6d1d6980219bd6a221f96eeb9bf9a232b8eb9
Steven Allen <steven@stebalien.com>
2019-01-25 07:31:27 -0800

    correctly decode the peer ID
    Otherwise, we cast the base64 encoded string to a peer ID (which isn't valid).

--- pubsub.go
+++ pubsub.go
@@ -6,11 +6,12 @@ import (
 	"github.com/libp2p/go-libp2p-peer"
 )
 
+// Message is a pubsub message.
 type Message struct {
-	From     peer.ID  `json:"from,omitempty"`
-	Data     []byte   `json:"data,omitempty"`
-	Seqno    []byte   `json:"seqno,omitempty"`
-	TopicIDs []string `json:"topicIDs,omitempty"`
+	From     peer.ID
+	Data     []byte
+	Seqno    []byte
+	TopicIDs []string
 }
 
 // PubSubSubscription allow you to receive pubsub records that where published on the network.
@@ -34,10 +35,28 @@ func (s *PubSubSubscription) Next() (*Message, error) {
 
 	d := json.NewDecoder(s.resp.Output)
 
-	var r Message
-	err := d.Decode(&r)
+	var r struct {
+		From     []byte   `json:"from,omitempty"`
+		Data     []byte   `json:"data,omitempty"`
+		Seqno    []byte   `json:"seqno,omitempty"`
+		TopicIDs []string `json:"topicIDs,omitempty"`
+	}
 
-	return &r, err
+	err := d.Decode(&r)
+	if err != nil {
+		return nil, err
+	}
+
+	from, err := peer.IDFromBytes(r.From)
+	if err != nil {
+		return nil, err
+	}
+	return &Message{
+		From:     from,
+		Data:     r.Data,
+		Seqno:    r.Seqno,
+		TopicIDs: r.TopicIDs,
+	}, nil
 }
 
 // Cancel cancels the given subscription.
