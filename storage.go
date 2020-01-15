6f86a030eff29379e7ce949a63a03d66f7406d01
liujiaxun2008 <jimmy.liu@tron.network>
2020-01-15 15:46:04 +0800

    perf: add storage upload length and update storage upload status response struct

--- storage.go
+++ storage.go
@@ -4,26 +4,41 @@ import (
 	"context"
 )
 
+type StorageUploadOpts = func(*RequestBuilder) error
+
 type storageUploadResponse struct {
 	ID string
 }
 
 type shard struct {
-	Price  int64
-	Host   string
-	Status string
+	ContractId string
+	Price      int64
+	Host       string
+	Status     string
 }
 
 type Storage struct {
 	Status   string
+	Message  string
 	FileHash string
 	Shards   map[string]shard
 }
 
+// Set storage upload time.
+func StorageLength(length int) StorageUploadOpts {
+	return func(rb *RequestBuilder) error {
+		rb.Option("storage-length", length)
+		return nil
+	}
+}
+
 // Storage upload api.
-func (s *Shell) StorageUpload(hash string) (string, error) {
+func (s *Shell) StorageUpload(hash string, options ...StorageUploadOpts) (string, error) {
 	var out storageUploadResponse
 	rb := s.Request("storage/upload", hash)
+	for _, option := range options {
+		_ = option(rb)
+	}
 	return out.ID, rb.Exec(context.Background(), &out)
 }
 
